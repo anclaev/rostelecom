a96bc95db9d650bf34acad69def8f5c66e09dbf1
Artem Samarin <iahugo@yandex.ru>
2024-04-19 15:12:20 +0300

    Переключение языка в меню.

--- components/modules/Header/Menu/index.tsx
+++ components/modules/Header/Menu/index.tsx
@@ -1,10 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
+'use client'
 import { useCallback, useState } from 'react'
 import { useUnit } from 'effector-react'
 
+import { removeOverflowHiddenFromBody } from '@lib/utils/common'
+
 import { $menuIsOpen, closeMenu } from '@context/modals'
+import { setLang } from '@context/lang'
+
 import { useLang } from '@hooks/useLang'
 
-import { removeOverflowHiddenFromBody } from '@lib/utils/common'
+import { LANGS } from '@constants/lang'
 
 import './index.css'
 
@@ -16,18 +22,53 @@ const Menu = () => {
   const menuIsOpen = useUnit($menuIsOpen)
 
   const handleCloseMenu = useCallback(() => {
-    console.log('hi')
     removeOverflowHiddenFromBody()
     closeMenu()
   }, [])
 
+  const handleSwitchLang = useCallback((lang: LANGS) => {
+    setLang(lang)
+    localStorage.setItem('lang', JSON.stringify(lang))
+  }, [])
+
+  const handleSwitchLangToEn = useCallback(
+    () => handleSwitchLang(LANGS.EN),
+    [handleSwitchLang]
+  )
+
+  const handleSwitchLangToRu = useCallback(
+    () => handleSwitchLang(LANGS.RU),
+    [handleSwitchLang]
+  )
+
   return (
     <nav className={`nav-menu ${menuIsOpen ? 'open' : 'close'}`}>
-      <button
-        className={`btn-reset nav-menu__close ${menuIsOpen ? 'open' : ''}`}
-        onClick={handleCloseMenu}
-      />
-      <h1>Menu</h1>
+      <div className='container nav-menu__container'>
+        <img
+          src='/img/menu-bg.png'
+          alt='Menu background'
+          className={`nav-menu__bg ${menuIsOpen ? 'open' : ''}`}
+        />
+        <button
+          className={`btn-reset nav-menu__close ${menuIsOpen ? 'open' : ''}`}
+          onClick={handleCloseMenu}
+        />
+        <div className={`nav-menu__lang ${menuIsOpen ? 'open' : ''}`}>
+          <button
+            className={`btn-reset nav-menu__lang__btn ${lang === 'ru' ? 'lang-active' : ''}`}
+            onClick={handleSwitchLangToRu}
+          >
+            RU
+          </button>
+          <button
+            className={`btn-reset nav-menu__lang__btn ${lang === 'en' ? 'lang-active' : ''}`}
+            onClick={handleSwitchLangToEn}
+          >
+            EN
+          </button>
+        </div>
+        <h1>menu</h1>
+      </div>
     </nav>
   )
 }
